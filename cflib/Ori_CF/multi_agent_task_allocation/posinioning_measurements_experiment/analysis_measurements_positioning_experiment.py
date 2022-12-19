d48a2aedd3400bc2c2c28275c0c79b7675dd10a5
orimana2020 <ori.mana@gmail.com>
2022-12-19 20:35:18 +0200

    add difference configuration models

--- cflib/Ori_CF/multi_agent_task_allocation/posinioning_measurements_experiment/analysis_measurements_positioning_experiment.py
+++ cflib/Ori_CF/multi_agent_task_allocation/posinioning_measurements_experiment/analysis_measurements_positioning_experiment.py
@@ -46,8 +46,8 @@ def load_results(lps_dir, vicon_dir, fix_values,samples_num ,is_2d, threshold, n
     error_arr = np.array(error_arr)
     sigma_arr = np.array(sigma_arr)
     # limits
-    # print(np.min(pos_vicon_arr,axis=0))
-    # print(np.max(pos_vicon_arr,axis=0))
+    print(np.min(pos_vicon_arr,axis=0))
+    print(np.max(pos_vicon_arr,axis=0))
     return pos_lps_arr, pos_vicon_arr, error_arr, sigma_arr, nodes
 
 
@@ -69,9 +69,9 @@ def plot_sampled_data(error_arr, threshold, title, pos_vicon_arr, nodes):
 
 def exterpolate_3d(limits, resolution, pos_vicon_arr, error_arr):
     grid_x, grid_y, grid_z = np.mgrid[limits[0][0]:limits[0][1]:resolution, limits[1][0]:limits[1][1]:resolution, limits[2][0]:limits[2][1]:resolution ]
-    # interpolated_error = griddata(pos_vicon_arr, error_arr, (grid_x, grid_y, grid_z ), method='linear')
-    rbf4 = Rbf(pos_vicon_arr[:,0], pos_vicon_arr[:,1], pos_vicon_arr[:,2],error_arr, function="multiquadric", smooth=5)
-    interpolated_error = rbf4(grid_x, grid_y, grid_z)
+    interpolated_error = griddata(pos_vicon_arr, error_arr, (grid_x, grid_y, grid_z ), method='linear')
+    # rbf4 = Rbf(pos_vicon_arr[:,0], pos_vicon_arr[:,1], pos_vicon_arr[:,2],error_arr, function='linear')#, smooth=5)
+    # interpolated_error = rbf4(grid_x, grid_y, grid_z)
     return grid_x, grid_y, grid_z, interpolated_error    
 
 
@@ -109,16 +109,55 @@ def plot_histogram(error_arr, title):
     ax.set_xlabel('error [meter]')
     ax.set_title(title)
 
+def plot_difference(error1, error2, grid_x, grid_y, grid_z,nodes1, nodes2 ,difference_threshold=None):
+    diff = error2 - error1
+    factor = diff  / np.nanmax(np.abs(diff))
+    colors = []
+    for xi in range(factor.shape[0]):
+        for yi in range(factor.shape[1]):
+            for zi in range(factor.shape[2]):
+                fac = factor[xi,yi, zi]
+                if difference_threshold == None:
+                    
+                    if fac >= 0: # error2>error1 -> show red
+                        colors.append(np.array([fac, 0, 0,1]))
+                    elif fac < 0 : # error2<error1 -> show green
+                        colors.append( np.array([0,-fac, 0,1]))   
+                    else: #in case fac = np.nan
+                        colors.append(np.array([1,1, 1,0]))
+
+                else: 
+                    if diff[xi,yi, zi] < 0 and np.abs(diff[xi,yi, zi]) >= difference_threshold:
+                        colors.append( np.array([0,1, 0,1]))
+                    else:
+                        colors.append(np.array([1, 1, 1,0]))
+
+    colors = np.array(colors)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(grid_x, grid_y, grid_z,c=colors)
+    ax.scatter(nodes1[:,0],nodes1[:,1],nodes1[:,2],c='blue', s=100)
+    ax.scatter(nodes2[:,0],nodes2[:,1],nodes2[:,2],c='yellow', s=100)
+
+    # ax.scatter(nodes[:,0],nodes[:,1],nodes[:,2],c='blue', s=100)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title('difference')
+
+
+
+
 #general params
 
 error_2d = True
-threshold = 0.2
-limits = [[-0.32, 3.5], [-1.5,1.5], [0.2,2.2]]
-resolution = 0.3
+threshold = 0.25
+limits = [[-0.32, 3.5], [-1.5,1.5], [0.2,2]]
+resolution = 0.15
 
 # box config params:
 samples_num_exp1 = 81
-fix_values_exp1 = np.array([0.062,0.051,0.035])/2
+fix_values_exp1 = np.array([0.062,0.051,0.035])*0.5
 nodes1 = np.array([[1.00	,1.30	,0.23],[1.12	,1.40,	2.23],[1.06	,-1.14	,0.18],[1.13	,-1.32	,2.17],[-0.98	,-1.15	,0.16],[-0.84,	-1.28,	2.17],[-0.94	,1.36,	0.21],[-0.78	,1.44	,2.23]])
 optimal_lps_dir = 'cflib/Ori_CF/multi_agent_task_allocation/posinioning_measurements_experiment/results/optimal_config/LPS_optimal/lps_static_pos_optimal_config_'
 optimal_vicon_dir = 'cflib/Ori_CF/multi_agent_task_allocation/posinioning_measurements_experiment/results/optimal_config/vicon_optimal/vicon_optimal_config_'
@@ -126,23 +165,27 @@ title_exp1 = 'box configuration'
 
 # extended params
 samples_num_exp2 = 87
-fix_values_exp2 = np.array([0.062,0.051,0.035])/2
+fix_values_exp2 = np.array([0.062,0.051,0.035])*0.75
 nodes2 =  np.array([[1.00	,1.30	,0.23],[1.12	,1.40,	2.23],[1.06	,-1.14	,0.18],[1.13	,-1.32	,2.17],[-1,	-1.14	,0.12],[-2.31	,-1.30	,2.17],[-0.96,	1.38	,0.18],[-2.33	,1.45	,2.22]])
 extended_lps_dir = 'cflib/Ori_CF/multi_agent_task_allocation/posinioning_measurements_experiment/results/extended_config/LPS_extended/lps_static_pos_changed_config_'
 extended_vicon_dir =  'cflib/Ori_CF/multi_agent_task_allocation/posinioning_measurements_experiment/results/extended_config/vicon_extended/vicon_changed_config_'
 title_exp2 = 'extended configuration'
 
 # box config
-pos_lps_arr, pos_vicon_arr, error_arr, sigma_arr, nodes1 = load_results(lps_dir=optimal_lps_dir, vicon_dir=optimal_vicon_dir, fix_values=fix_values_exp1,samples_num=samples_num_exp1 ,is_2d=error_2d, threshold=threshold, nodes=nodes1)
-plot_sampled_data(error_arr, threshold, title_exp1, pos_vicon_arr, nodes1)
-grid_x, grid_y, grid_z, interpolated_error = exterpolate_3d(limits, resolution, pos_vicon_arr, error_arr)
-plot_exterpolate(interpolated_error, threshold, grid_x, grid_y, grid_z, nodes1, title_exp1)
+lps_1, vicon_1, error_1, sigma_1, nodes1 = load_results(lps_dir=optimal_lps_dir, vicon_dir=optimal_vicon_dir, fix_values=fix_values_exp1,samples_num=samples_num_exp1 ,is_2d=error_2d, threshold=threshold, nodes=nodes1)
+plot_sampled_data(error_1, threshold, title_exp1, vicon_1, nodes1)
+grid_x, grid_y, grid_z, interp_err_1 = exterpolate_3d(limits, resolution, vicon_1, error_1)
+plot_exterpolate(interp_err_1, threshold, grid_x, grid_y, grid_z, nodes1, title_exp1)
 
 
 # extended configuration
-pos_lps_arr, pos_vicon_arr, error_arr, sigma_arr, nodes2 = load_results(lps_dir=extended_lps_dir, vicon_dir=extended_vicon_dir, fix_values=fix_values_exp2,samples_num=samples_num_exp2 ,is_2d=error_2d, threshold=threshold, nodes=nodes2)
-plot_sampled_data(error_arr, threshold, title_exp2, pos_vicon_arr, nodes2)
-grid_x, grid_y, grid_z, interpolated_error = exterpolate_3d(limits, resolution, pos_vicon_arr, error_arr)
-plot_exterpolate(interpolated_error, threshold, grid_x, grid_y, grid_z, nodes2, title_exp2)
+lps_2, vicon_2, error_2, sigma_2, nodes2 = load_results(lps_dir=extended_lps_dir, vicon_dir=extended_vicon_dir, fix_values=fix_values_exp2,samples_num=samples_num_exp2 ,is_2d=error_2d, threshold=threshold, nodes=nodes2)
+plot_sampled_data(error_2, threshold, title_exp2, vicon_2, nodes2)
+grid_x, grid_y, grid_z, interpo_err_2 = exterpolate_3d(limits, resolution, vicon_2, error_2)
+plot_exterpolate(interpo_err_2, threshold, grid_x, grid_y, grid_z, nodes2, title_exp2)
 
+
+# diffence
+plot_difference(interp_err_1, interpo_err_2, grid_x, grid_y, grid_z ,nodes1, nodes2, difference_threshold=0.07)
+plot_histogram(interpo_err_2.flatten() - interp_err_1.flatten(), title='diff')
 plt.show()
